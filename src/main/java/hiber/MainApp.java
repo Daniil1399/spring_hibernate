61ea5245567571a5bd1b2b5e708b750254713240
Daniil1399 <danil1399danil@yandex.ru>
2021-09-19 16:35:08 +0300

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,46 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.CarService;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+//      CarService carService = context.getBean(CarService.class);
+//
+//      carService.add(new Car("Toyota", 1));
+//      carService.add(new Car("Lada", 2));
+//      carService.add(new Car("hyundai", 3));
+//      carService.add(new Car("nissan", 4));
+
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("nissan", 4)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("lada", 5)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("taz", 4)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("gaz", 4)));
+
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+ user.getCar());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
---
+++ src/main/java/hiber/service/CarServiceImpl.java
@@ -0,0 +1,27 @@
+package hiber.service;
+
+import hiber.dao.CarDao;
+import hiber.model.Car;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService{
+
+    private CarDao carDao;
+
+    @Transactional
+    @Override
+    public void add(Car car) {
+        carDao.add(car);
+
+    }
+
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    @Override
+    public List<Car> ListCars() {
+        return carDao.listCars();
+    }
+}
